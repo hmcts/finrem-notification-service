5f56bf644a81fe41f16baa53d1befaff4b93062a
Padmaja Ramisetti <padmaja.r.uk@gmail.com>
2018-10-23 16:39:50 +0100

    FR_414_Setup_notification_micro_service (#1)
    * FR_414_Setup_notification_micro_service
    
    - added build.gradle
    - added infrastructure files
    - added CNP pipeline files
    - added readme
    
    * FR_414_Setup_notification_micro_service
    
    - added gradlew
    
    * FR_414_Setup_notification_micro_service
    
    - added travis yml

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/notifications/NotificationApplication.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.finrem.notifications;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@EnableRetry
+public class NotificationApplication {
+    @Value("${http.request.read.timeout}")
+    private int httpRequestReadTimeout;
+
+    @Value("${http.request.connect.timeout}")
+    private int httpRequestConnectTimeout;
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    private List<HttpMessageConverter<?>> getMessageConverters() {
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        messageConverters.add(new MappingJackson2HttpMessageConverter());
+        return messageConverters;
+    }
+
+    private ClientHttpRequestFactory requestFactory() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(httpRequestConnectTimeout);
+        requestFactory.setReadTimeout(httpRequestReadTimeout);
+
+        return requestFactory;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(getMessageConverters());
+        restTemplate.setRequestFactory(requestFactory());
+
+        return restTemplate;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+}
