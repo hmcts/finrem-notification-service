fd684ac4d808094386be4c76af0f2531e423a1eb
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-10-25 16:01:44 +0100

    FR_414_Setup_notification_micro_service
    - CNP Jenkins pipeline setup

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,70 +1,55 @@
 locals {
-  aseName         = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
-  local_env       = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  local_ase       = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.aseName}"
-  idam_s2s_url    = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
+  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+
+  idam_s2s_url                       = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+
+
+  previewVaultName = "${var.reform_team}-aat"
+  nonPreviewVaultName = "${var.reform_team}-${var.env}"
+  vaultName = "${var.env == "preview" ? local.previewVaultName : local.nonPreviewVaultName}"
+  vaultUri = "${data.azurerm_key_vault.finrem_ns_key_vault.vault_uri}"
+
+  asp_name = "${var.env == "prod" ? "div-dgs-prod" : "${var.raw_product}-${var.env}"}"
+  asp_rg = "${var.env == "prod" ? "div-dgs-prod" : "${var.raw_product}-${var.env}"}"
 }
 
-module "finrem-notification-service" {
-  source          = "git@github.com:hmcts/moj-module-webapp?ref=master"
-  product         = "${var.product}-${var.app}"
-  location        = "${var.location}"
-  env             = "${var.env}"
-  ilbIp           = "${var.ilbIp}"
-  subscription    = "${var.subscription}"
-  is_frontend     = false
-  capacity        = "${var.capacity}"
+module "div-dgs" {
+  source                          = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
+  product                         = "${var.product}-${var.component}"
+  location                        = "${var.location}"
+  env                             = "${var.env}"
+  ilbIp                           = "${var.ilbIp}"
+  subscription                    = "${var.subscription}"
+  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
+  capacity                        = "${var.capacity}"
+  is_frontend                     = false
+  common_tags                     = "${var.common_tags}"
+  asp_name                        = "${local.asp_name}"
+  asp_rg                          = "${local.asp_rg}"
 
   app_settings = {
+    REFORM_SERVICE_NAME                                   = "${var.reform_service_name}"
+    REFORM_TEAM                                           = "${var.reform_team}"
     REFORM_ENVIRONMENT                                    = "${var.env}"
     AUTH_PROVIDER_SERVICE_CLIENT_BASEURL                  = "${local.idam_s2s_url}"
     AUTH_PROVIDER_SERVICE_CLIENT_MICROSERVICE             = "${var.auth_provider_service_client_microservice}"
-    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.vault_generic_secret.auth-provider-service-client-key.data["value"]}"
+    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.azurerm_key_vault_secret.finrem-notification-service-s2s-key.value}"
     AUTH_PROVIDER_SERVICE_CLIENT_TOKENTIMETOLIVEINSECONDS = "${var.auth_provider_service_client_tokentimetoliveinseconds}"
-    AUTH_PROVIDER_HEALTH_URI                              = "${local.idam_s2s_url}/health"
-    UK_GOV_NOTIFY_API_KEY                                 = "${data.vault_generic_secret.uk-gov-notify-api-key.data["value"]}"
-    UK_GOV_NOTIFY_EMAIL_TEMPLATES                         = "${var.uk_gov_notify_email_templates}"
-    UK_GOV_NOTIFY_EMAIL_TEMPLATE_VARS                     = "${var.uk_gov_notify_email_template_vars}"
-    IDAM_API_BASEURL                                      = "${var.idam_api_baseurl}"
-    IDAM_API_HEALTH_URI                                   = "${var.idam_api_baseurl}/health"
   }
 }
 
-# region save DB details to Azure Key Vault
-module "key-vault" {
-  source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
-  name                = "${var.product}-${var.app}-${var.env}"
-  product             = "${var.product}"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.finrem-notification-service.resource_group_name}"
-  # dcd_cc-dev group object ID
-  product_group_object_id = "94ac8962-b614-441b-aa4c-9be878a6bf17"
+data "azurerm_key_vault" "finrem_ns_key_vault" {
+  name                = "${local.vaultName}"
+  resource_group_name = "${local.vaultName}"
 }
 
-provider "vault" {
-  // # tactical vault - for example: use `data "vault_generic_secret" "s2s_secret" {`
-  address = "https://vault.reform.hmcts.net:6200"
+data "azurerm_key_vault_secret" "finrem-notification-service-s2s-key" {
+  name      = "finrem-notification-service-s2s-secret"
+  vault_uri = "${data.azurerm_key_vault.finrem_ns_key_vault.vault_uri}"
 }
 
-data "vault_generic_secret" "auth-provider-service-client-key" {
-  path = "secret/${var.vault_section}/ccidam/service-auth-provider/api/microservice-keys/finrem-notification-service"
+data "azurerm_key_vault_secret" "gov-uk-notification-key" {
+  name      = "gov-uk-notification-key"
+  vault_uri = "${data.azurerm_key_vault.finrem_ns_key_vault.vault_uri}"
 }
-
-data "vault_generic_secret" "uk-gov-notify-api-key" {
-  path = "secret/${var.vault_section}/finrem/notify/api_key"
-}
-
-resource "azurerm_key_vault_secret" "auth-provider-service-client-key" {
-  name      = "auth-provider-service-client-key"
-  value     = "${data.vault_generic_secret.auth-provider-service-client-key.data["value"]}"
-  vault_uri = "${module.key-vault.key_vault_uri}"
-}
-
-resource "azurerm_key_vault_secret" "uk-gov-notify-api-key" {
-  name      = "uk-gov-notify-api-key"
-  value     = "${data.vault_generic_secret.uk-gov-notify-api-key.data["value"]}"
-  vault_uri = "${module.key-vault.key_vault_uri}"
-}
-
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,69 +1,74 @@
-variable "product" {
-  type = "string"
-  default = "fr"
+variable "reform_service_name" {
+  default = "ns"
 }
 
-variable "app" {
-  type = "string"
-  default = "dss"
+variable "reform_team" {
+  default = "finrem"
 }
 
-variable "location" {
-  type    = "string"
-  default = "UK South"
+variable "capacity" {
+  default = "1"
+}
+
+variable "component" {
+  type = "string"
 }
 
 variable "env" {
   type = "string"
 }
 
-variable "infrastructure_env" {
-  default     = "dev"
-  description = "Infrastructure environment to point to"
-}
-
-variable "subscription" {
+variable "product" {
   type = "string"
 }
 
-variable "tenant_id" {
-  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+variable "raw_product" {
+  default = "finrem"
 }
+
+variable "client_id" {
+  description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
+}
+
+variable "tenant_id" {}
 
 variable "jenkins_AAD_objectId" {
-  type = "string"
+  type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable "vault_section" {
-  default = "test"
+variable "appinsights_instrumentation_key" {
+  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
+  default = ""
 }
 
-variable "ilbIp"{}
-
-variable "capacity" {
-  default = "1"
+variable "idam_s2s_url_prefix" {
+  default = "rpe-service-auth-provider"
 }
 
-
-//if possible move this to application.property
 variable "auth_provider_service_client_microservice" {
-  default = "finrem_notification_store"
+  default = "finrem-notification-service"
 }
 
-//if possible move this to application.property
 variable "auth_provider_service_client_tokentimetoliveinseconds" {
   default = "900"
 }
 
-variable "uk_gov_notify_email_templates" {
-  type = "string"
+variable "finrem_notification_service_api_health_endpoint" {
+  default = "/health"
 }
 
-variable "uk_gov_notify_email_template_vars" {
-  type = "string"
+variable "subscription" {}
+
+variable "location" {
+  type    = "string"
+  default = "UK South"
 }
 
-variable "idam_api_baseurl" {
-  type = "string"
+variable "ilbIp" {}
+
+variable "vault_env" {}
+
+variable "common_tags" {
+  type = "map"
 }
