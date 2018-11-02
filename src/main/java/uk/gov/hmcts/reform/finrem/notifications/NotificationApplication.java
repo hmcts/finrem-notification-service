2318ac06affb45f7d57200ae11a74b557d1fdc15
Padmaja Ramisetti <padmaja.r.uk@gmail.com>
2018-11-02 12:01:50 +0000

    FR_414_CNP (#11)
    * FR_414_CNP
    
    - added piTest
    - modified project structure
    
    * FR_414_CNP
    
    - enabled health check show-details
    
    * FR_414_CNP
    
    - enabled health check show-details
    
    * FR_414_CNP
    
    - changed the jar name
    
    * FR_414_CNP
    
    - changed the jar name
    
    * FR_414_CNP
    
    - enabled slack notifications
    - update vault key

--- src/main/java/uk/gov/hmcts/reform/finrem/notifications/NotificationApplication.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/notifications/NotificationApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class})
 public class NotificationApplication {
 
     public static void main(String[] args) {
