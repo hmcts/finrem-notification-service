f9b1f9cf4ec928e1e13e4989bc70d27a0a4315b2
Padmaja Ramisetti <padmaja.r.uk@gmail.com>
2018-11-19 12:29:37 +0000

    FR_483_swagger_doc (#15)
    * FR_483_swagger_doc
    
    - sonar exclusions
    - swagger enabled
    - unit test properties updated.
    - docker compose updated.
    
    * FR_483_swagger_doc
    
    - Added dependency supression for guava

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/notifications/config/SwaggerConfiguration.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.finrem.notifications.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import uk.gov.hmcts.reform.finrem.notifications.NotificationApplication;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration implements WebMvcConfigurer {
+
+    @Value("${documentation.swagger.enabled}")
+    private boolean swaggerEnabled;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(NotificationApplication.class.getPackage().getName()))
+                .build()
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Financial Remedy Notification Service")
+                .description("Given a notification request, This service will send notification emails"
+                        + " to the destination email address")
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        if (swaggerEnabled) {
+            registry.addResourceHandler("/swagger-ui.html**")
+                    .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+            registry.addResourceHandler("/webjars/**")
+                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+
+}
--- src/test/java/uk/gov/hmcts/reform/finrem/notifications/client/EmailClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/notifications/client/EmailClientTest.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 public class EmailClientTest {
 
     @Value("${uk.gov.notify.api.key}")
