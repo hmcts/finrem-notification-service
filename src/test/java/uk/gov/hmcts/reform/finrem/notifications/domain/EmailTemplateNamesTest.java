1d0c4f9ac470329aaa92d994f5b07576a7f3e1be
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-11-26 15:34:53 +0000

    FR_399_assign_to_judge
    - added tests

--- src/test/java/uk/gov/hmcts/reform/finrem/notifications/domain/EmailTemplateNamesTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/notifications/domain/EmailTemplateNamesTest.java
@@ -10,4 +10,13 @@ public class EmailTemplateNamesTest {
     public void shouldReturnHWFSuccessful() {
         assertEquals("FR_HWF_SUCCESSFUL", EmailTemplateNames.FR_HWF_SUCCESSFUL.name());
     }
+
+    @Test
+    public void shouldReturnAssignToJudge() {
+        assertEquals("FR_ASSIGNED_TO_JUDGE", EmailTemplateNames.FR_ASSIGNED_TO_JUDGE.name());
+    }
+    @Test
+    public void shouldReturnConsentOrderMade() {
+        assertEquals("FR_CONSENT_ORDER_MADE", EmailTemplateNames.FR_CONSENT_ORDER_MADE.name());
+    }
 }
