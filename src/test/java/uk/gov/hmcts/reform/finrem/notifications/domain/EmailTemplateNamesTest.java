33506c33a968e6097dd3b985cb626a98dd887638
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-11-26 15:39:41 +0000

    FR_399_assign_to_judge
    - fixed checkstyle

--- src/test/java/uk/gov/hmcts/reform/finrem/notifications/domain/EmailTemplateNamesTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/notifications/domain/EmailTemplateNamesTest.java
@@ -15,6 +15,7 @@ public class EmailTemplateNamesTest {
     public void shouldReturnAssignToJudge() {
         assertEquals("FR_ASSIGNED_TO_JUDGE", EmailTemplateNames.FR_ASSIGNED_TO_JUDGE.name());
     }
+
     @Test
     public void shouldReturnConsentOrderMade() {
         assertEquals("FR_CONSENT_ORDER_MADE", EmailTemplateNames.FR_CONSENT_ORDER_MADE.name());
