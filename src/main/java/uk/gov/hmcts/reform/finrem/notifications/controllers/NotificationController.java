f35743b35119b2ff487188b61160fdf500d65f4b
Padmaja Ramisetti <padmaja.r.uk@gmail.com>
2018-11-27 11:15:44 +0000

    Fr 399 notify assigned to judge (#18)
    * FR_399_assign_to_judge
    
    - draft version
    
    * FR_399_assign_to_judge
    
    - changed the properties
    
    * FR_399_assign_to_judge
    
    - added tests
    
    * FR_399_assign_to_judge
    
    - fixed checkstyle
    
    * FR_399_assign_to_judge
    
    - removed log

--- src/main/java/uk/gov/hmcts/reform/finrem/notifications/controllers/NotificationController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/notifications/controllers/NotificationController.java
@@ -14,10 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.finrem.notifications.domain.HwfSuccessfulNotificationRequest;
+import uk.gov.hmcts.reform.finrem.notifications.domain.NotificationRequest;
 import uk.gov.hmcts.reform.finrem.notifications.service.EmailService;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_ASSIGNED_TO_JUDGE;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_CONSENT_ORDER_MADE;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_HWF_SUCCESSFUL;
 
 @RestController
 @RequestMapping(path = "/notify")
@@ -39,10 +42,48 @@ public class NotificationController {
             @ApiParam(value = "The fixtures contains case reference number,"
                     + " solicitorReferenceNumber and the email address that will receive "
                     + "the notification that the HWF is successful and all are mandatory")
-            final HwfSuccessfulNotificationRequest notificationRequest) {
+            final NotificationRequest notificationRequest) {
         log.info("Received request for notification email for HWFSuccessful. Auth token: {}, Notification request : {}",
                 authToken, notificationRequest);
-        emailService.sendHwfSuccessfulConfirmationEmail(notificationRequest);
+        emailService.sendConfirmationEmail(notificationRequest, FR_HWF_SUCCESSFUL);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping(path = "/assign-to-judge", consumes = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "send e-mail for a case assigned to judge.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Assigned to Judge e-mail sent successfully")})
+    public ResponseEntity<Void> sendEmailAssignToJudge(
+            @RequestHeader("Authorization")
+            @ApiParam(value = "JWT authorisation token issued by IDAM", required = true) final String authToken,
+            @RequestBody
+            @ApiParam(value = "The fixtures contains case reference number,"
+                    + " solicitorReferenceNumber and the email address that will receive "
+                    + "the notification that a case is assigned to judge and all are mandatory")
+            final NotificationRequest notificationRequest) {
+        log.info("Received request for notification email for Case assigned to Judge. Auth token: {},"
+                        + " Notification request : {}",
+                authToken, notificationRequest);
+        emailService.sendConfirmationEmail(notificationRequest, FR_ASSIGNED_TO_JUDGE);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping(path = "/consent-order-made", consumes = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "send e-mail for Consent order Made.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Consent order made e-mail sent successfully")})
+    public ResponseEntity<Void> sendEmailConsentOrderApproved(
+            @RequestHeader("Authorization")
+            @ApiParam(value = "JWT authorisation token issued by IDAM", required = true) final String authToken,
+            @RequestBody
+            @ApiParam(value = "The fixtures contains case reference number,"
+                    + " solicitorReferenceNumber and the email address that will receive "
+                    + "the notification that a consent order is made and all are mandatory")
+            final NotificationRequest notificationRequest) {
+        log.info("Received request for notification email for consent order made. Auth token: {}, "
+                        + "Notification request : {}",
+                authToken, notificationRequest);
+        emailService.sendConfirmationEmail(notificationRequest, FR_CONSENT_ORDER_MADE);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
