883539211fa8d60df2d29f87d219b47f0bd564ae
Padmaja Ramisetti <padmaja.r.uk@gmail.com>
2018-12-04 10:19:15 +0000

    FR_401_notification && FR_403_consent_order_notification (#20)
    - implemented rest endpoints
    - tests

--- src/test/java/uk/gov/hmcts/reform/finrem/notifications/domain/EmailTemplateNamesTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/notifications/domain/EmailTemplateNamesTest.java
@@ -3,21 +3,36 @@ package uk.gov.hmcts.reform.finrem.notifications.domain;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_ASSIGNED_TO_JUDGE;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_CONSENT_ORDER_AVAILABLE;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_CONSENT_ORDER_MADE;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_CONSENT_ORDER_NOT_APPROVED;
+import static uk.gov.hmcts.reform.finrem.notifications.domain.EmailTemplateNames.FR_HWF_SUCCESSFUL;
 
 public class EmailTemplateNamesTest {
 
     @Test
     public void shouldReturnHWFSuccessful() {
-        assertEquals("FR_HWF_SUCCESSFUL", EmailTemplateNames.FR_HWF_SUCCESSFUL.name());
+        assertEquals("FR_HWF_SUCCESSFUL", FR_HWF_SUCCESSFUL.name());
     }
 
     @Test
     public void shouldReturnAssignToJudge() {
-        assertEquals("FR_ASSIGNED_TO_JUDGE", EmailTemplateNames.FR_ASSIGNED_TO_JUDGE.name());
+        assertEquals("FR_ASSIGNED_TO_JUDGE", FR_ASSIGNED_TO_JUDGE.name());
     }
 
     @Test
     public void shouldReturnConsentOrderMade() {
-        assertEquals("FR_CONSENT_ORDER_MADE", EmailTemplateNames.FR_CONSENT_ORDER_MADE.name());
+        assertEquals("FR_CONSENT_ORDER_MADE", FR_CONSENT_ORDER_MADE.name());
+    }
+
+    @Test
+    public void shouldReturnConsentOrderNotApproved() {
+        assertEquals("FR_CONSENT_ORDER_NOT_APPROVED", FR_CONSENT_ORDER_NOT_APPROVED.name());
+    }
+
+    @Test
+    public void shouldReturnConsentOrderAvailable() {
+        assertEquals("FR_CONSENT_ORDER_AVAILABLE", FR_CONSENT_ORDER_AVAILABLE.name());
     }
 }
