5108ea439808d9a60a04a0a024f862499a365460
devadathpt <devadathpt@gmail.com>
2019-02-13 16:43:15 +0000

    Testing notification URL

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/notification/NotificationTests.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/notification/NotificationTests.java
@@ -1,24 +1,12 @@
 package uk.gov.hmcts.reform.finrem.functional.notification;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
-import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.finrem.functional.IntegrationTestBase;
-
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SerenityRunner.class)
 public class NotificationTests extends IntegrationTestBase {
@@ -36,53 +24,50 @@ public class NotificationTests extends IntegrationTestBase {
     private String environment;
 
 
-
     @Test
     public void verifyNotifyAssignToJudgeTestIsOkay() {
 
-        validatePostSuccessForNotification( NOTIFY_ASSIGN_TO_JUDGE,"assignedToJudge.json");
+        validatePostSuccessForNotification(NOTIFY_ASSIGN_TO_JUDGE, "assignedToJudge.json");
 
     }
 
     @Test
     public void verifyNotifyConsentOrderAvailableTestIsOkay() {
 
-        validatePostSuccessForNotification( CONSENT_ORDER_AVAILABLE,"consentOrderAvailable.json");
+        validatePostSuccessForNotification(CONSENT_ORDER_AVAILABLE, "consentOrderAvailable.json");
 
     }
 
     @Test
     public void verifyNotifyConsentOrderMadeTestIsOkay() {
 
-        validatePostSuccessForNotification( CONSENT_ORDER_MADE,"consentOrderMade.json");
+        validatePostSuccessForNotification(CONSENT_ORDER_MADE, "consentOrderMade.json");
 
     }
 
     @Test
     public void verifyNotifyConsentOrderNotApprovedTestIsOkay() {
 
-        validatePostSuccessForNotification( CONSENT_ORDER_NOT_APPROVED,"consentOrderNotApproved.json");
+        validatePostSuccessForNotification(CONSENT_ORDER_NOT_APPROVED, "consentOrderNotApproved.json");
 
     }
 
     @Test
     public void verifyNotifyHwfSuccessfulTestIsOkay() {
 
-        validatePostSuccessForNotification( HWF_SUCCESSFUL_API_URI,"hwfSuccessfulEmail.json");
+        validatePostSuccessForNotification(HWF_SUCCESSFUL_API_URI, "hwfSuccessfulEmail.json");
 
     }
 
 
-    private void validatePostSuccessForNotification(String url,String jsonFileName) {
+    private void validatePostSuccessForNotification(String url, String jsonFileName) {
 
-        System.out.println("URL is " + "=========" + notificationUrl + "========="
-                + url + "==============" + "env is :" + environment);
         SerenityRest.given()
                 .relaxedHTTPSValidation()
                 .headers(utils.getNewHeaders())
                 .body(utils.getJsonFromFile(jsonFileName))
                 .when().post(notificationUrl + url)
-                .then().assertThat().statusCode(200);
+                .then().assertThat().statusCode(HttpStatus.OK.value());
 
     }
 
