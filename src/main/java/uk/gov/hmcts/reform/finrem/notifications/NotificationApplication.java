a52f2c7b88642c1fc60f519d0271a5d8ddfd43df
Padmaja Ramisetti <padmaja.r.uk@gmail.com>
2019-05-30 10:23:51 +0100

    FR-819_health_check_cache_and_cleanup (#38)
    * FR-819
    
    - cleanup dependencies
    
    * FR-819
    
    - Fix CVE-2019-12086

--- src/main/java/uk/gov/hmcts/reform/finrem/notifications/NotificationApplication.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/notifications/NotificationApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
 
-@SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class})
+@SpringBootApplication
 public class NotificationApplication {
 
     public static void main(String[] args) {
