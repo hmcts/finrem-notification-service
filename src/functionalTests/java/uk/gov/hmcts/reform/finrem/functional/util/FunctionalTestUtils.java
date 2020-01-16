2bee3af3b70fc1e265e92b2109bc6d3e193678bd
John O'Neill <johnoneill546@gmail.com>
2020-01-16 14:48:33 +0000

    Bumped checkstyle version and resolved issues

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/util/FunctionalTestUtils.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/util/FunctionalTestUtils.java
@@ -3,13 +3,10 @@ package uk.gov.hmcts.reform.finrem.functional.util;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.util.ResourceUtils;
 import uk.gov.hmcts.reform.finrem.functional.TestContextConfiguration;
-
 
 import java.io.File;
 import java.io.IOException;
@@ -18,7 +15,6 @@ import java.nio.file.Files;
 @ContextConfiguration(classes = TestContextConfiguration.class)
 @Component
 public class FunctionalTestUtils {
-
 
     public String getJsonFromFile(String fileName) {
         try {
@@ -30,11 +26,8 @@ public class FunctionalTestUtils {
         }
     }
 
-
     public Headers getNewHeaders() {
         return Headers.headers(
                 new Header("Content-Type", ContentType.JSON.toString()));
     }
-
-
 }
