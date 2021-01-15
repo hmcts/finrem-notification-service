94718d2a9e093395c57c894179f0425359933225
vianvi <30834324+vianvi@users.noreply.github.com>
2021-01-15 11:18:06 +0000

    remove html report generation

--- security.sh
+++ security.sh
@@ -5,5 +5,5 @@ export LANG=C.UTF-8
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001
 cat zap.out
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium report -o /zap/api-report.html -f html --exit-code False 
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
 cp *.* functional-output/
