4c14e44a6910086028315f66a241fcf38521b72e
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-06-20 15:46:11 +0100

    add logging

--- src/main/java/uk/gov/hmcts/reform/finrem/notifications/controllers/ConsentedNotificationController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/notifications/controllers/ConsentedNotificationController.java
@@ -51,6 +51,7 @@ public class ConsentedNotificationController {
             final NotificationRequest notificationRequest) {
         log.info("Received request for notification email for HWFSuccessful. Case ID : {}",
                  notificationRequest.getCaseReferenceNumber());
+        log.info("Notification request for caseID {}: {}", notificationRequest.getCaseReferenceNumber(), notificationRequest);
         emailService.sendConfirmationEmail(notificationRequest, FR_HWF_SUCCESSFUL);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
